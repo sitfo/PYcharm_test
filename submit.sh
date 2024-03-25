169cfb71574e029adf34780228f6e6d53b97b06a
sitfo <47027133+sitfo@users.noreply.github.com>
2024-03-25 19:35:08 +0000

    update shell file

--- submit.sh
+++ submit.sh
@@ -6,8 +6,15 @@
 
 set -e
 
-module purge; module load bluebear
+module purge
+module load bluebear
 module load bear-apps/2022a
 module load PyTorch/2.0.1-foss-2022a-CUDA-11.7.0
 
-pip install --user -r requirements.txt&&python /rds/homes/j/jxl1872/PYcharm_test/hugging_face_files/model_train_longform.py
+pip install --user -r requirements.txt
+
+# Navigate to the directory of the script
+cd "$(dirname "$0")"
+
+# Run the Python script in the subfolder
+python hugging_face_files/model_train_longform.py
