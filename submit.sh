950b9df8cab82caa6c8b425bd2a538430a3acccf
sitfo <47027133+sitfo@users.noreply.github.com>
2024-03-25 21:50:37 +0000

    update shell file

--- submit.sh
+++ submit.sh
@@ -10,6 +10,7 @@ module purge
 module load bluebear
 module load bear-apps/2022a
 module load PyTorch/2.0.1-foss-2022a-CUDA-11.7.0
+module load cuDNN/8.4.1.50-CUDA-11.7.0
 
 pip install --user -r requirements.txt
 
