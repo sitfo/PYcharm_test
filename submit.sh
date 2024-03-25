55811c1576d3eb9eddf003eca1ea2e702d05f44c
sitfo <47027133+sitfo@users.noreply.github.com>
2024-03-25 19:30:57 +0000

    update shell file

--- submit.sh
+++ submit.sh
@@ -10,4 +10,4 @@ module purge; module load bluebear
 module load bear-apps/2022a
 module load PyTorch/2.0.1-foss-2022a-CUDA-11.7.0
 
-pip install --user -r requirements.txt&&python /rds/homes/j/jxl1872/PYcharm_test/hugging_face_files/model_train_longformer.py
+pip install --user -r requirements.txt&&python /rds/homes/j/jxl1872/PYcharm_test/hugging_face_files/model_train_longform.py
