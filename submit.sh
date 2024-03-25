ad79c53bd7784b90061232f3447b27ceaeecd880
sitfo <47027133+sitfo@users.noreply.github.com>
2024-03-25 19:47:58 +0000

    update shell file

--- submit.sh
+++ submit.sh
@@ -13,11 +13,4 @@ module load PyTorch/2.0.1-foss-2022a-CUDA-11.7.0
 
 pip install --user -r requirements.txt
 
-# Set the absolute path to the directory containing the Python script
-SCRIPT_DIR="/rds/homes/j/jxl1872/PYcharm_test/hugging_face_files"
-
-# Navigate to the directory
-cd "$SCRIPT_DIR"
-
-# Run the Python script
-python model_train_longform.py
+python /rds/homes/j/jxl1872/PYcharm_test/hugging_face_files/model_train_longform.py
